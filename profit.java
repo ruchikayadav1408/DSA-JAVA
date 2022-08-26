2a858cd7e0b596a677d153b539e4d324d00a35e4
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2022-08-26 13:44:40 +0530

    Update profit.java

--- profit.java
+++ profit.java
@@ -1,3 +1,10 @@
+//You  are  given  an  array prices where prices[i] is  the  price  of  a  given  stock  on  the ith day.
+ //   Return the maximum profit you can achieve from this transaction.
+  //  If you cannot achieve any profit, return 0.
+ //   Example 1:Input:prices = [7, 1, 5, 3, 6,  4]  
+  //  Output:   5 
+   //     Explanation:Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+
 public class profit{
     public static int buyandsell(int prices[]){
         int maxprofit=0;
