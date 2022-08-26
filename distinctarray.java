508d57374d9a722cf36e08e7f7b1828c6a150b68
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2022-08-26 13:40:24 +0530

    Update distinctarray.java

--- distinctarray.java
+++ distinctarray.java
@@ -1,3 +1,9 @@
+//Given an integer array nums, return true if any value appears at least twice in the array,
+//and return false if every element is distinct.
+// Example:Input:nums = [1, 2, 3, 1]
+ //Output:   true
+
+
 import java.util.*;
 
 public class distinctarray {
