7632b3d5dd952d5764e8eb6fa76e1ea670c14014
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2022-08-28 20:14:56 +0530

    Update matrixspiral.java

--- matrixspiral.java
+++ matrixspiral.java
@@ -1,3 +1,8 @@
+//return all elements of the matrix in spiral order
+//Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+//Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ 
+
 import java.util.*;
 public class matrixspiral {
     
