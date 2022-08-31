f796759ac4d0f8724eb7bc7b2a55ad0bc3dce163
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2022-08-31 13:20:19 +0530

    Create fibonacci.java

---
+++ LEETCODE/fibonacci.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static  int fib(int n) {
+        if(n==0 || n==1){
+            return n;
+            
+        }
+       
+        int sum =fib(n-1)+ fib(n-2);
+        
+        return sum;
+    }
+    
+}
