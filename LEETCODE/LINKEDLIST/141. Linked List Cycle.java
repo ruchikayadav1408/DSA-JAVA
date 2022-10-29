c11e0c76469d3006521f05b14f420d962b3020b5
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2022-10-30 00:59:19 +0530

    Create 141. Linked List Cycle.java

---
+++ LEETCODE/LINKEDLIST/141. Linked List Cycle.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/linked-list-cycle
+///Input: head = [3,2,0,-4], pos = 1
+//Output: true
+//Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+public class Solution {   
+ 
+  
+    public boolean hasCycle(ListNode head) {
+     ListNode slow =head;
+        ListNode fast=head;
+          while(fast!=null && fast.next!=null){
+              slow=slow.next;
+              fast=fast.next.next;
+              
+              if(slow==fast){
+                  return true;
+              }
+              
+          }
+        return false;
+    }
+
+
+}
