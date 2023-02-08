89615d5b37d69123e2f80bf2f129a7f991becb67
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-02-08 17:38:21 +0530

    Add files via upload

---
+++ backtracking/gridways.java
@@ -0,0 +1,22 @@
+public class gridways {
+    public static int grid(int i, int j , int n, int m){
+        //base case
+        if(i==n-1 && j==m-1){//condition for last cell
+            return 1;
+
+        }
+        else if(i==n || j==m){ //boundry condition
+            return 0;
+        }
+    
+        int w1=grid(i+1, j, n, m);
+        int w2=grid(i, j+1, n, m);
+        int totalways=w1+w2;
+        return totalways;
+    }
+    public static void main(String args[]){
+        int n=3, m=3;
+        System.out.println(grid(0,0, n,m));
+    }
+    
+}
