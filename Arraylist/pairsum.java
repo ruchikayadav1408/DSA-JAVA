eabf054144bc30d4807a699aaa178f4a6cdf6eb2
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-02-11 20:22:49 +0530

    SORTED ARRAY

--- Arraylist/pairsum.java
+++ Arraylist/pairsum.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class pairsum {
 
-    // BRUTE FORCE
+    // BRUTE FORCE o(n^2)
     public static boolean sum(ArrayList<Integer> list, int target){
         for(int i=0;i<list.size();i++){
           for(int j=i+1;j<list.size();j++){
@@ -14,7 +14,7 @@ public class pairsum {
         return false;
     }
 
-    //optimized solution 2 pointer approach
+    //optimized solution 2 pointer approach - o(n)
     public static boolean printsum1(ArrayList<Integer> list, int target) {
       int lp=0;
       int rp=list.size()-1;
