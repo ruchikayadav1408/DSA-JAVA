c3bff5b7c3bf60ebdd33483ea387ffa1beb0cc86
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-02-21 22:30:19 +0530

    Update trapingrainwater.java

--- trapingrainwater.java
+++ trapingrainwater.java
@@ -1,5 +1,5 @@
-//Given n non-negative integers representing an elevation map where the width of each bar is 1, 
-//compute how much water it can trap after raining.
+//Given n non-negative integers representing an elevation map where 
+//the width of each bar is 1, compute how much water it can trap after raining.
 //  Example 1:Input:height = [0, 1, 0,  2, 1, 0, 1, 3, 2, 1, 2, 1]
 //  Output:   6 
 //Explanation:The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
