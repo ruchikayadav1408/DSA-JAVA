08f35ea7d94c1e993ed7092649052f0519ae827d
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-02-26 13:07:32 +0530

    java collection framework

---
+++ stack/stackjcf.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class stackjcf{
+    public static void main(String args[]){
+        Stack<Integer> s= new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+         while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+
+         }
+    }
+}
