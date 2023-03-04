d432565d5b8a853c137bd261553c3b02a4f48aa8
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-03-04 15:13:45 +0530

    Java collection framework

---
+++ queuejcf.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.LinkedList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class queuejcf {
+
+    public static void main(String args[]){
+        Queue<Integer> q= new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+         }
+    }
+    
+}
