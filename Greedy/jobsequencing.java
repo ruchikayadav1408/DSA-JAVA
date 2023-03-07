ab5230e2da3f33705122e3d8c9004751b903db6d
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-03-07 19:38:16 +0530

    Add files via upload

---
+++ Greedy/jobsequencing.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class jobsequencing {
+    static class Job {
+        int id;
+        int deadline;
+        int profit;
+
+        public Job(int id, int deadline, int profit) {
+            this.id = id;
+            this.deadline = deadline;
+            this.profit = profit;
+        }
+    }
+
+    public static void main(String args[]) {
+        int jobsInfo[][] = {{4, 20}, {1, 10}, {1, 40}, {1, 30}};
+        ArrayList<Job> jobs = new ArrayList<>();
+
+        for(int i=0; i<jobsInfo.length; i++) {
+            jobs.add(new Job(i, jobsInfo[i][0], jobsInfo[i][1]));
+        }
+
+        Collections.sort(jobs, (a, b) -> b.profit - a.profit);
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        ans.add(jobs.get(0).id);
+        int time = 1;
+        for(int i=1; i<jobs.size(); i++) {
+            Job j = jobs.get(i);
+            if(time < j.deadline) {
+                ans.add(j.id);
+                time++;
+            }
+        } 
+        System.out.println("max jobs: "+ ans.size());
+        for(int i=0; i<ans.size(); i++) {
+            System.out.print(ans.get(i)+" ");
+        }
+
+        System.out.println();
+    }
+}
+
