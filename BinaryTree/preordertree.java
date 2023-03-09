e8c7a2412f2c4364d83f24552ef7497131eeb294
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-03-09 21:48:00 +0530

    Update preordertree.java

--- BinaryTree/preordertree.java
+++ BinaryTree/preordertree.java
@@ -59,6 +59,41 @@ public class preordertree {
              System.out.print(root.data + " ");
             
            }
+          // level order traverse
+           public static void levelorder(Node root){
+            if(root== null){
+              return ;
+
+            }
+            Queue<Node> q=new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+             while(!q.isEmpty()){
+              Node currnode=q.remove();
+              if(currnode==null){
+                System.out.println();
+                if(q.isEmpty()){
+                  break;
+                }
+                else{
+                  q.add(null);
+                }
+              }
+              else{
+                System.out.print(currnode.data + " ");
+                if(currnode.left != null){
+                  q.add(currnode.left);
+
+                }
+                if(currnode.right != null){
+                  q.add(currnode.right);
+                }
+              }
+             }
+
+
+           }
           }
            public static void main(String args[]) {
             int nodes[]={1,2,4, -1,-1, 5, -1, -1,3, -1,6,-1, -1};
