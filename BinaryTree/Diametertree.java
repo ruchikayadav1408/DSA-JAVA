646388471d002202ada25abeeb526540c6edb1c3
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-03-16 16:45:36 +0530

    Update Diametertree.java

--- BinaryTree/Diametertree.java
+++ BinaryTree/Diametertree.java
@@ -24,6 +24,32 @@ public class Diametertree{
              return Math.max(selfdiam , Math.max(rightDiam, leftDiam));
 
          }
+    
+    
+         //APPROACH - 2 O(N)
+
+         static class Info{
+            int ht;
+            int diam;
+
+            public Info(int diam , int ht){
+                this.diam= diam;
+                this.ht= ht;
+            }    
+             }
+
+             public static Info diameter2(Node root){
+                if(root== null){
+                    return new Info(0, 0);
+                }
+                Info leftInfo =diameter2(root.left);
+                Info rightInfo= diameter2(root.right);
+
+                int diam=Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht+rightInfo.ht+1);
+                int ht=Math.max(leftInfo.ht, rightInfo.ht)+1;
+
+                 return new Info(diam , ht);
+             }
     public static void main(String args[]) {
 /*
                     1
@@ -41,7 +67,8 @@ public class Diametertree{
         root.right.left = new Node(6);
         root.right.right = new Node(7);
      
-       System.out.print("Diameter of tree : " + diameter(root));
+       System.out.print("Diameter of tree : " + diameter(root));   //approach 1 - O(N^2)
+         System.out.println("diamrter of tree : " + diameter2(root).diam);    //Approach 2 - O(N)
     }
     
 }
