c44f01a869157d911a41abb64944238a3046a28b
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-03-27 21:53:29 +0530

    Create priorityJCF.java

---
+++ PriorityQueue/priorityJCF.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class priorityJCF{
+
+  // JAVA COLLECTION FRAMEWORK (JCF)
+  public static void main(String args[]) {
+    PriorityQueue<Integer> pq= new PriorityQueue<>();
+    pq.add(1);
+    pq.add(3);
+    pq.add(7);
+    pq.add(4);
+
+     while(!pq.isEmpty()){
+        System.out.println(pq.peek());
+        pq.remove();
+     }
+
+  }  
+
+}
