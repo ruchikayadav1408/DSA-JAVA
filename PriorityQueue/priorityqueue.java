6c47ee708e3bd4c56bc0b372a30b637eb24c8c77
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-03-27 21:52:04 +0530

    compare objects

--- PriorityQueue/priorityqueue.java
+++ PriorityQueue/priorityqueue.java
@@ -1,16 +1,29 @@
 import java.util.*;
-public class priorityqueue{
-  
-  // JAVA COLLECTION FRAMEWORK (JCF)
+import java.util.jar.Attributes.Name;
+public class priorityqueue {
+    static class Student implements Comparable<Student>{
+        int rank;
+        String name;
+
+        public Student(String name, int rank){
+            this.name= name;
+            this.rank=rank;
+        }
+    
+    @Override
+    public int compareTo(Student s2){
+        return this.rank - s2.rank;
+    }
+}
   public static void main(String args[]) {
-    PriorityQueue<Integer> pq= new PriorityQueue<>();
-    pq.add(1);
-    pq.add(3);
-    pq.add(7);
-    pq.add(4);
+    PriorityQueue<Student> pq= new PriorityQueue<>();
+    pq.add(new Student("A", 3));
+    pq.add(new Student("R", 10));
+    pq.add(new Student("C", 1));
+    pq.add(new Student("D", 6));
 
      while(!pq.isEmpty()){
-        System.out.println(pq.peek());
+        System.out.println(pq.peek().name + " -> " + pq.peek().rank);
         pq.remove();
      }
     
