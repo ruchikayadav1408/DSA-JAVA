918a4850f3c1fa16cabcf9ac7397e540b0ff9b5c
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-07-15 23:24:52 +0530

    Create secondlargest.java

---
+++ GFG/secondlargest.java
@@ -0,0 +1,26 @@
+//https://practice.geeksforgeeks.org/problems/second-largest3735/0
+class Solution {
+    int print2largest(int arr[], int n) {
+        // code here
+        
+         int max = -1;
+        int seclarg=-1;
+  
+        for(int i=0;i<arr.length;i++){
+            if(max<arr[i]){
+            seclarg=max;
+            max=arr[i];
+            
+            }
+        }
+         for(int i=0;i<arr.length;i++){
+        if(arr[i]>=seclarg && arr[i]!= max){
+            seclarg= arr[i];
+        }
+       
+        
+    }
+    return seclarg;
+    }   
+
+}
