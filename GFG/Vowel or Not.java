3cc922aa0aa0bdfe0c2ac5b5135d4d74be03cc80
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-07-22 23:33:57 +0530

    Create Vowel or Not.java

---
+++ GFG/Vowel or Not.java
@@ -0,0 +1,14 @@
+
+
+//User function Template for Java
+class Solution{
+    String isVowel(char c){
+        // code here
+        if(c=='A' || c=='E'||c=='I'||c=='O'||c=='U'||c=='a'
+        ||c=='e'||c=='i'||c=='o'||c=='u'){
+            return "YES";
+        }
+        
+        return "NO";
+    }
+}
