620ef02d691f8b94ef699f35be40d6f6a36fb5bd
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-07-23 22:02:20 +0530

    Create Searching an element in a sorted array.java

---
+++ GFG/Searching an element in a sorted array.java
@@ -0,0 +1,15 @@
+//https://practice.geeksforgeeks.org/problems/who-will-win-1587115621/0
+class Solution{
+    static int searchInSorted(int arr[], int N, int K)
+    {
+        
+        // Your code here
+        
+        for(int i=0;i<N;i++){
+            if(arr[i]== K){
+                return 1;
+            }
+        }
+        return -1;
+    }
+}
