f95d4502beb8690b0b42d420ce75e5b9cb02a0c6
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-07-23 21:59:15 +0530

    Create Sort an array of 0s, 1s and 2s.java

---
+++ GFG/Sort an array of 0s, 1s and 2s.java
@@ -0,0 +1,11 @@
+//https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/0
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here 
+        Arrays.sort(a);
+     
+    }
+}
